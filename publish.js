782d9485a6ebcf58034445fff6611e42ce0bdd3c
Michael Kühnel <m.kuehnel@micromata.de>
2016-11-12 01:41:03 +0100

    Generate folder for version if versionSwitcher === true

--- publish.js
+++ publish.js
@@ -551,6 +551,11 @@ exports.publish = function(taffyData, opts, tutorials) {
         }
     });
 
+	// update outdir if necessary, then create outdir
+    var packageInfo = ( find({kind: 'package'}) || [] ) [0];
+    if (packageInfo && packageInfo.version && conf.versionSwitcher) {
+        outdir = path.join( outdir, (packageInfo.version) );
+    }
     fs.mkPath(outdir);
 
     // copy the template's static files to outdir
