2ce01055dfcfbc9d32abbcec174bf02bc70841f7
Michael Kühnel <m.kuehnel@micromata.de>
2016-11-22 23:50:29 +0100

    Implement filtering version via semver ranges

--- versionSwitcher/index.js
+++ versionSwitcher/index.js
@@ -4,14 +4,18 @@ var fs = require('jsdoc/fs');
 var path = require('jsdoc/path');
 var Promise = require('bluebird');
 var gitSemverTags = Promise.promisify(require('git-semver-tags'));
+var semver = require('semver');
+var _ = require('lodash');
 
 /**
- * [exports description]
+ * Generates the index.html, the versionsSwitcher directory and its contents in
+ * the output directory.
  * @param  {String} outputDir      Path to the output folder for the generated documentation
  * @param  {String} currentVersion Current version from package.json
  * @param  {String} pageTitle      Used as content of the <title> element of the page
+ * @param  {Array} semverRanges    Array of semver ranges from JSDoc config property versionSwitcher.versions
  */
-module.exports = function (outputDir, currentVersion, pageTitle) {
+module.exports = function (outputDir, currentVersion, pageTitle, semverRanges) {
     var staticJSDocTemplateDir = path.normalize(path.join(__dirname, '../static'));
     var versionSwitcherOutputDir;
     var staticVersionSwitcherDir;
@@ -37,9 +41,34 @@ module.exports = function (outputDir, currentVersion, pageTitle) {
     fs.copyFileSync(path.normalize(staticVersionSwitcherDir + '/versionSwitcher.css'), versionSwitcherOutputDir);
     fs.copyFileSync(path.normalize(staticVersionSwitcherDir + '/versionSwitcher.js'), versionSwitcherOutputDir);
 
-    // Get versions from Git tags and generate {outputDir}/versionSwitcher/data.js
-    gitSemverTags().then(function(versions){
-        versionSwitcherData.versions = versions;
+    // Get versions from Git tags
+    gitSemverTags().then(function(semverTags){
+        return semverTags;
+    }).then(function(semverTags) {
+
+        // Filter by versions if versionSwitcher.versions
+        if (semverRanges) {
+            versionSwitcherData.versions = _.chain(semverRanges)
+                                                .map(function(semverRange){
+                                                    return _.filter(semverTags, function(semverTag){
+                                                        return semver.satisfies(semverTag, semverRange) === true;
+                                                    })
+                                                })
+                                                .flatten()
+                                                .uniq()
+                                                .sort(function(a, b) {
+                                                    if (a < b) {
+                                                        return -1;
+                                                    }
+                                                    return 1;
+                                                })
+                                                .reverse()
+                                                .value();
+        } else {
+            versionSwitcherData.versions = semverTags;
+        }
+
+        // Generate {outputDir}/versionSwitcher/data.js
         fs.writeFileSync(path.normalize(versionSwitcherOutputDir + '/data.js'), 'var data = ' + JSON.stringify(versionSwitcherData) + ';');
     });
 
